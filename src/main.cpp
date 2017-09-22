fa046b6c104b07c7392b5f6a80da8ca25bd2dc6c
haidyn <haidyn1988@gmail.com>
2017-09-22 22:01:07 +1000

    feat: travel down center of lane at speed limit fix: data file location address

--- src/main.cpp
+++ src/main.cpp
@@ -170,7 +170,7 @@ int main() {
   vector<double> map_waypoints_dy;
 
   // Waypoint map to read from
-  string map_file_ = "../data/highway_map.csv";
+  string map_file_ = "data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
 
@@ -237,7 +237,17 @@ int main() {
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
-
+            
+            double dist_inc = 0.44;
+            for(int i = 0; i < 50; i++)
+            {
+              double next_s = car_s + (i+1) * dist_inc;
+              double next_d = 6;
+              vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              
+              next_x_vals.push_back(xy[0]); //car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+              next_y_vals.push_back(xy[1]); //car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            }
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
